fa989c794b4ac8bc0e727b59c5aa2fa7f2b04b6e
Parth Patel <parth@cappital.co>
2020-03-29 21:26:19 +0530

    config UI added

---
+++ src/components/config/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./config.css"
+
+export default function () {
+  return (
+    <div className="config-wrapper">
+      <div className="colors"><div className="circle"></div></div>
+      <div className="colors"><div className="circle"></div></div>
+      <div className="colors"><div className="circle"></div></div>
+      <div className="colors"><div className="circle"></div></div>
+      <div className="colors"><div className="circle"></div></div>
+    </div>
+  )
+}
